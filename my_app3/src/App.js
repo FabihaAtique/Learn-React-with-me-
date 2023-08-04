560e28c95ac6d341788d4e5752964f7e94a501a3
FabihaAtique <atiquefabiha@gmail.com>
2023-08-04 08:55:14 +0500

    routes

--- my_app3/src/App.js
+++ my_app3/src/App.js
@@ -1,50 +1,63 @@
 import { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import Alert from "./components/Alert";
+
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+
+
 function App() {
-  // dark mode of the whole page will be controlled by the state variable in the app.js 
-  //by default the mode is light 
-  const [Mode, setMode]=  useState('light');
+  // dark mode of the whole page will be controlled by the state variable in the app.js
+  //by default the mode is light
+  const [Mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) =>{
-    setAlert({     //alert is an object 
-      msg : message,
+  const showAlert = (message, type) => {
+    setAlert({
+      //alert is an object
+      msg: message,
       type: type,
-    })
+    });
     setTimeout(() => {
       setAlert(null);
     }, 3000);
-  }
+  };
 
-  const togglemode = () =>{
-    if(Mode === "light"){
+  const togglemode = () => {
+    if (Mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      showAlert("dark mode has been enabled" , "success");
-    }
-    else{
+      showAlert("dark mode has been enabled", "success");
+    } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("light mode has been enabled", "success");
     }
-  }
+  };
   return (
     <>
-            {/* we have made the component for navbar so that the app.js file does not get too mixed up  */}
-            {/* i can use this component for other projects  */}
-            {/* taking props for the title  */}
-      <Navbar title="Fabcode" AboutText="About content" Mode ={Mode} togglemode = {togglemode}/>
-      <Alert Alert = {alert}/>
-      <div className="container my-3" >
-
-        <Textform heading="Enter your details " Mode= {Mode}/>
-        {/* <About/> */}
-
-      </div>
+      <Router>
+        {/* we have made the component for navbar so that the app.js file does not get too mixed up  */}
+        {/* i can use this component for other projects  */}
+        {/* taking props for the title  */}
+        <Navbar
+          title="Fabcode"
+          AboutText="About content"
+          Mode={Mode}
+          togglemode={togglemode}
+        />
+        <Alert Alert={alert} />
+        <div className="container my-3">    
+        <Routes> {/* Use Routes instead of Switch */}
+        {/* exact is used so that there is exact matching  */}
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<Textform heading="Enter your details " Mode={Mode} />} />
+        </Routes>  
+        </div>
+      </Router>
     </>
   );
 }
--- my_app3/src/components/Navbar.js
+++ my_app3/src/components/Navbar.js
@@ -1,15 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom"; // Use Link instead of link
 
 export default function Navbar(props) {
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-${props.Mode} bg-${props.Mode}`}
-    >
+    <nav className={`navbar navbar-expand-lg navbar-${props.Mode} bg-${props.Mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/About">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -24,52 +23,64 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/About">
                 {props.AboutText}
-              </a>
+              </Link>
             </li>
             {/* Add the dropdown menu class to the parent ul */}
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="/">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="/">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item" href="/">
-                  Something else here
-                </a>
-              </li>
-            </ul>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </Link>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/">
+                    Action
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/">
+                    Another action
+                  </Link>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/">
+                    Something else here
+                  </Link>
+                </li>
+              </ul>
+            </li>
             {/* Continue with other navigation items */}
             <li className="nav-item">
-              <a className="nav-link disabled" href="/">
+              <Link className="nav-link disabled" to="/">
                 Disabled
-              </a>
-             </li>
+              </Link>
+            </li>
           </ul>
           <form className="d-flex" role="search">
             <input placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">
               Search
-             </button> 
-            {/* a condition is used to set the text color to either light or dark  */}
-            <div className={`form-check form-switch text-${props.Mode ==="light" ? "dark" : "light"}`}>
+            </button>
+            {/* linkcondition is used to set the text color to either light or dark  */}
+            <div className={`form-check form-switch text-${props.Mode === "light" ? "dark" : "light"}`}>
               <input
-                className="form-check-input "
+                className="form-check-input"
                 type="checkbox"
                 role="switch"
                 id="flexSwitchCheckDefault"
